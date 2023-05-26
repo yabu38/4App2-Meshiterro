bf60623069876cedbb6360d528860f3586984731
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-05-26 13:05:44 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   get "homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit]
 end
